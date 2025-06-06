d8d1a69fae3ff176b81ecd264c67a466476485ec
Moksha-S <moksha@simformsolutions.com>
2025-06-06 14:40:45 +0530

    feat: add Tailwind CSS module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-05-15",
+  compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt", "@nuxt/eslint", "vuetify-nuxt-module"],
+  modules: ['@pinia/nuxt', '@nuxt/eslint', 'vuetify-nuxt-module', '@nuxtjs/tailwindcss'],
   vuetify: {
     // Vuetify configuration
     theme: {
-      defaultTheme: "light",
+      defaultTheme: 'light',
       themes: {
         light: {
           colors: {
-            primary: "#1976D2",
-            secondary: "#424242",
-            accent: "#82B1FF",
-            error: "#FF5252",
-            info: "#2196F3",
-            success: "#4CAF50",
-            warning: "#FFC107",
+            primary: '#1976D2',
+            secondary: '#424242',
+            accent: '#82B1FF',
+            error: '#FF5252',
+            info: '#2196F3',
+            success: '#4CAF50',
+            warning: '#FFC107',
           },
         },
       },
@@ -26,13 +26,13 @@ export default defineNuxtConfig({
   pages: true,
   vite: {
     define: {
-      "process.env.DEBUG": false,
+      'process.env.DEBUG': false,
     },
     server: {
       hmr: {
         clientPort: 3000,
         port: 24678,
-        host: "0.0.0.0",
+        host: '0.0.0.0',
       },
       watch: {
         usePolling: true,
